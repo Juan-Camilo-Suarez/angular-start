8095e3b00db42f04b78f16ae01b0d77022ba1fad
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-08-01 12:24:11 +0300

    import and export objects

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Configuracion } from './models/configuracion';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'aprendiendo JS - Angular';
   public mostrar_videojuegos: boolean = true;
+  public descripcion: string;
+  public config;
+
+  //constructor
+  constructor(){
+    //usando la variable de config importada
+    this.config = Configuracion;
+    this.title = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+  }
 
   ocultarVideoJuegos(value:boolean){
     this.mostrar_videojuegos = value;
---
+++ aprendiendo-angular/src/app/models/configuracion.ts
@@ -0,0 +1,7 @@
+//export 
+export var Configuracion = {
+    color: "red",
+    fondo: "#ccc",
+    titulo: "aplicacion de angular",
+    descripcion: "**aprendiendo angular"
+}
