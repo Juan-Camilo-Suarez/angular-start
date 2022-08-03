9005e8428b1bc952d4766d9db6c8ec019159d2e5
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-08-03 12:01:57 +0300

    directiva *ngfor

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -7,20 +7,38 @@ import { Zapatilla } from "../models/zapatilla";
 })
 export class ZapatillasComponent implements OnInit {
     public titulo: String = "componente de zapatillas";
-    public zapatillas: Array<Zapatilla>; 
+    public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
 
     constructor(){
+        this.marcas = new Array();
         this.zapatillas = [
 
             new Zapatilla("superstar", 500, "blanco", " adidas", true)
-            ,new Zapatilla("yezzy", 600, "verde", "nike", true)
-            ,new Zapatilla("nike", 75, "azul", "nike", true)
+            ,new Zapatilla("yezzy", 600, "verde", "nike", false)
+            ,new Zapatilla("nike", 75, "azul", "puma", true)
 
         ];
     }
     ngOnInit() {
         console.log(this.zapatillas);
+        this.getMarcas();
     }
+
+    getMarcas(){
+		this.zapatillas.forEach((zapatilla, index) =>{
+			//ṕara no agregar marcas repetidas
+			if(this.marcas.indexOf(zapatilla.marca) < 0){
+				this.marcas.push(zapatilla.marca);
+
+			}
+			
+		});
+        console.log(this.marcas);
+    }
+
+
+
 }
         
